23eabd892b53b5f6cfae24c5c764b0b13a2147a0
Meng Zhong Kuan <kuanmz@gmail.com>
2023-02-25 17:03:42 -0600

    Added editor cmd commands

--- back-end/utils/utils.py
+++ back-end/utils/utils.py
@@ -6,13 +6,8 @@
 from settings import db
 from definitions import MongoDBStaticData
 
-
 def mongodb_cmd_cases(cmd, cmd_id, new_data, session=None):
     query = list(db.listingsAndReviews.find().limit(10))
-
-    #print(query)
-
-    print(cmd)
 
     query_data_indexed = {}
     for item in query:
@@ -38,8 +33,31 @@ def mongodb_cmd_cases(cmd, cmd_id, new_data, session=None):
         except Exception as e:
             raise Exception("Such ID is not present in the database!")
         return query_data_indexed
-
-        # There is an error
+    # There is an error
     else:
         raise Exception("None of the cases were matched")
 
+def execute_command(command):
+    supported_commands = ["insert", "find", "update", "delete"]
+    # Extract the collection name and command from user input
+    try:
+        _, collection_name, command_name = command.split('.')
+    except ValueError:
+        return "Invalid command format: Please use the format 'db.collection.command'"
+
+    # Check if the collection exists 
+    if collection_name not in db.list_collection_names():
+        return f"Collection '{collection_name}' does not exist in the database"
+
+    # Get the collection object
+    collection = db[collection_name]
+
+     # Check if the command is valid for the collection
+    if any(supported_command in command_name for supported_command in supported_commands):
+        # Execute the command using PyMongo
+        result = list(eval(f"collection.{command_name}"))
+
+        # Return the result as a string
+        return "\n".join([str(doc) for doc in result])
+  
+    return f"Unsupported command '{command_name}' for collection '{collection_name}'. Valid commands are: {valid_commands}"
