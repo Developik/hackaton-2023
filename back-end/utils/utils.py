6a29f143090fd4d5fc809ff5808bbcb8eeb2b89b
Andrii Provozin <andrii.provozin@alexatranslations.com>
2023-02-25 16:33:12 -0500

    Update utils spaces

--- back-end/utils/utils.py
+++ back-end/utils/utils.py
@@ -21,23 +21,23 @@ def mongodb_cmd_cases(cmd, cmd_id, new_data, session=None):
         query_data_indexed[item_id] = item
 
     if cmd == "INSERT":
-            assert (new_data.get("_id") is not None)
-            new_data_adj = {new_data['_id']: new_data}
-            _id = new_data_adj[new_data['_id']].pop('_id')
-            query_data_indexed[_id] = new_data_adj
-            return query_data_indexed
+        assert (new_data.get("_id") is not None)
+        new_data_adj = {new_data['_id']: new_data}
+        _id = new_data_adj[new_data['_id']].pop('_id')
+        query_data_indexed[_id] = new_data_adj
+        return query_data_indexed
     elif cmd == "FIND":
-            return query_data_indexed
+        return query_data_indexed
     elif cmd == "UPDATE":
-            assert (cmd_id is not None)
-            query_data_indexed[cmd_id] = new_data
-            return query_data_indexed
+        assert (cmd_id is not None)
+        query_data_indexed[cmd_id] = new_data
+        return query_data_indexed
     elif cmd == "DELETE":
-            try:
-                del query_data_indexed[cmd_id]
-            except Exception as e:
-                raise Exception("Such ID is not present in the database!")
-            return query_data_indexed
+        try:
+            del query_data_indexed[cmd_id]
+        except Exception as e:
+            raise Exception("Such ID is not present in the database!")
+        return query_data_indexed
 
         # There is an error
     else:
